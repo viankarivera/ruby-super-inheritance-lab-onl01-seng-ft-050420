13f7358c2461d3f8b18a23fc888697f83153b475
Vianka Rivera <viankalrivera@gmail.com>
2020-05-20 20:02:57 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,12 +2,11 @@ class ChattyStudent < Student
 
   def hello
     super
-    "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let mem just tell you who died..."
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let mem just tell you who died..."
   end
 
   def raise_hand
-    super
-    "Pick me!" * 10
+    10.times {super}
   end 
 
 end
